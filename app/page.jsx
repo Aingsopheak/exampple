3992a70d823ebd4f8065586103a0fea1499a7ab9
aingsopheak <aingsopheak13@gmail.com>
2023-08-02 14:11:51 +0700

    refract

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>
+        The quick brown fox jumps over the lazy dog is an English-language
+        pangram—a phrase that contains all of the letters of the alphabet. It is
+        used to show fonts and to test typewriters and computer keyboards, and
+        in other applications involving all of the letters in the English
+        alphabet.
+      </p>
+    </div>
+  );
+};
+
+export default Homepage;
